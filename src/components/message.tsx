44604018d4977ffca95fc02893afdd6ce852df4b
HyunJaeyeon <bomul0109@gmail.com>
2023-10-26 20:13:13 +0900

    comment: add component descriptions
    - layout
    - message

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,8 +1,10 @@
+//채팅 말풍선 컴포넌트
+
 import { cls } from "../libs/utils";
 
 interface MessageProps {
   message: string;
-  reversed?: boolean;
+  reversed?: boolean; //내 말풍선,프로필-> 상대랑 반대로 뒤집기
   avatarUrl?: string;
 }
 
@@ -15,11 +17,11 @@ export default function Message({
     <div
       className={cls(
         "flex  items-start",
-        reversed ? "flex-row-reverse space-x-reverse" : "space-x-2"
+        reversed ? "flex-row-reverse space-x-reverse" : "space-x-2",
       )}
     >
-      <div className="w-8 h-8 rounded-full bg-slate-400" />
-      <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+      <div className="h-8 w-8 rounded-full bg-slate-400" />
+      <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
         <p>{message}</p>
       </div>
     </div>
