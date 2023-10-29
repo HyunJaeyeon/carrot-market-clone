7607a1248b789c65995cab09a16bd895c54ef7fd
HyunJaeyeon <bomul0109@gmail.com>
2023-10-29 23:46:42 +0900

    feat: add department, submit

--- src/pages/forms/jobApplication.tsx
+++ src/pages/forms/jobApplication.tsx
@@ -20,4 +20,91 @@ interface IForm {
   email: string;
 }
 
-export default function JobForms() {}
+export default function JobForms() {
+  //useForm에서 가져오기
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm<IForm>({ mode: "all" });
+
+  //data 받아오기 성공
+  const onValid = (data: IForm) => {
+    console.log(data);
+  };
+  //data 받아오기 실패, Error
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+
+  return (
+    <>
+      <header className="p-5">
+        <h1 className="text-center text-2xl font-bold">Job Application Form</h1>
+      </header>
+      <form
+        onSubmit={handleSubmit(onValid, onInvalid)}
+        className="border-2 border-stone-800 bg-pink-200 p-5"
+      >
+        {/* checkDepartment: required*/}
+        <div>
+          {/* title */}
+          <h1 className="py-1 font-semibold">
+            What department do you want to work for?
+          </h1>
+          {/* labels */}
+          <div className="grid gap-2">
+            <label>
+              <input
+                {...register("checkDepartment", {
+                  //required
+                  required: "*required",
+                })}
+                type="radio" //라디오박스로
+                value="sales" //각각 value 지정해줘야 함
+              />
+              Sales
+            </label>
+            <label>
+              <input
+                {...register("checkDepartment", {
+                  required: "*required",
+                })}
+                type="radio"
+                value="marketing"
+              />
+              Marketing
+            </label>
+            <label>
+              <input
+                {...register("checkDepartment", {
+                  required: "*required",
+                })}
+                type="radio"
+                value="accounting"
+              />
+              Accounting
+            </label>
+            <label>
+              <input
+                {...register("checkDepartment", {
+                  required: "*required",
+                })}
+                type="radio"
+                value="customer service"
+              />
+              Customer Service
+            </label>
+            {errors.checkDepartment?.message}
+          </div>
+        </div>
+        {/* submit */}
+        <div>
+          <input type="submit" value="Give me this Job" />
+        </div>
+      </form>
+    </>
+  );
+}
